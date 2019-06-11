579241ba122ea277848a84b737e20263d36d573d
asifisa <49895737+asifisa@users.noreply.github.com>
2019-06-11 09:20:06 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+ 
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	printf("Positive");
+	else if(a<0)
+	printf("Negative");
+	else
+	printf("Zero");
+	// your code goes here
+	return 0;
+}
